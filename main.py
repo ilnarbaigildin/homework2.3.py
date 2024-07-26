225661fcbb6288bc3c909131881bea13df78146f
ilnarbaigildin <baga2012@mail.ru>
2024-07-26 23:36:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ name = 'Ilnar'
 print('Name:', name)
 age = 33
 print('Age:', age)
-print('New age:', age + (1))
+age = age +1
+print('New age:', age)
 is_student = 'True'
 print('Is Student:', is_student)
